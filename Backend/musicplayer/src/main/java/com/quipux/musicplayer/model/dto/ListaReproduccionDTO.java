8499bd21bd0e9a7dc9811651a9ba2c06b7be4987
Brian León Hoyos <brian.leon@uao.edu.co>
2024-03-23 15:12:03 -0500

    Commit_3
    Construccion DTOs

--- Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/dto/ListaReproduccionDTO.java
+++ Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/dto/ListaReproduccionDTO.java
@@ -1,4 +1,42 @@
 package com.quipux.musicplayer.model.dto;
 
+import java.util.List;
+
 public class ListaReproduccionDTO {
+    private String nombre;
+    private String descripcion;
+    private List<CancionDTO> canciones;
+
+    public ListaReproduccionDTO() {
+    }
+
+    public ListaReproduccionDTO(String nombre, String descripcion, List<CancionDTO> canciones) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.canciones = canciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<CancionDTO> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(List<CancionDTO> canciones) {
+        this.canciones = canciones;
+    }
 }
