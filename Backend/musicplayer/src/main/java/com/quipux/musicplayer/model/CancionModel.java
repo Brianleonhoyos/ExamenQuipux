fcb945b164439c99ffa48f0b1649868f69d23800
Brian León Hoyos <brian.leon@uao.edu.co>
2024-03-23 16:23:10 -0500

    Commit_4
    Elaboracion modelos para relacion con base de datos

--- Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/CancionModel.java
+++ Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/CancionModel.java
@@ -1,4 +1,87 @@
 package com.quipux.musicplayer.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cancion")
 public class CancionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCancion", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @Column(name = "artista", nullable = false)
+    private String artista;
+
+    @Column(name = "album", nullable = false)
+    private String album;
+
+    @Column(name = "anno", nullable = false)
+    private int anno;
+
+    @Column(name = "genero", nullable = false)
+    private String genero;
+
+    @ManyToOne
+    @JoinColumn(name = "lista_reproduccion_id")
+    private ListaReproduccionModel listaReproduccionModel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public ListaReproduccionModel getListaReproduccionModel() {
+        return listaReproduccionModel;
+    }
+
+    public void setListaReproduccionModel(ListaReproduccionModel listaReproduccionModel) {
+        this.listaReproduccionModel = listaReproduccionModel;
+    }
 }
--- Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/ListaReproduccionModel.java
+++ Backend/musicplayer/src/main/java/com/quipux/musicplayer/model/ListaReproduccionModel.java
@@ -1,4 +1,56 @@
 package com.quipux.musicplayer.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "lista_reproduccion")
 public class ListaReproduccionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idListaReproduccion", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "listaReproduccionModel", cascade = CascadeType.ALL)
+    private List<CancionModel> canciones;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<CancionModel> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(List<CancionModel> canciones) {
+        this.canciones = canciones;
+    }
 }
